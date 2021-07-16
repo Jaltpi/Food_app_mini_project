c4952bb81203e658589e9df26dcebec56bee530a
jaltpi <jenielthomas@gmail.com>
2021-07-16 17:55:28 +0100

    Put functions in for menus' order

--- mini_project.py
+++ mini_project.py
@@ -1,7 +1,7 @@
 # mini_project_week_3
 
-from module import main_menu, orders_menu, products_menu, couriers_menu, create_new, current_list, edit_item, deletion_item
-from module import orders_menu
+from module import delete_order, main_menu, orders_menu, products_menu, couriers_menu, create_new, current_list, edit_item, deletion_item, update_existing_order
+from module import orders_menu, show_orders, create_order, update_order_status, update_existing_order, delete_order
 
 LOADING = "Loading Main Menu..."
 INVALID = "Invalid selection, returning back to main menu."
@@ -107,120 +107,24 @@ while application_on:
             
         elif fourth_choice == "1":
             # Show current orders
-             if len(orders_list) == 0:
-                    print("Your orders list is currently empty.")
-             else:
-                print(f"The current orders list is:")
-                for placement, order in enumerate(orders_list):
-                    print(placement, order)
-                
+            show_orders(orders_list)
+                            
         elif fourth_choice == "2":
             # Create an order
-           try:
-               customers_name = input("Please enter a name for the order: ").title()
-               customers_address = input("Please enter an address for delivery: ").upper()
-               customers_contact = input("Please enter a valid U.K number for us to contact you: ")
-               
-               if customers_name == "" or customers_address == "" or customers_contact == "": # Checks for empty strings in contact details
-                   raise ValueError
-               elif customers_contact.isdigit() == False or len(customers_contact) != 11: # checks to confirm if number entered is valid phone length and is all digits
-                   raise ValueError
-               
-           except ValueError:
-               print("Some details necessary for your order are missing, please try again.")   
-           except Exception as e:
-               print(f"Error: {e}. Please try again.")
-           else:
-               for identification, courier in enumerate(couriers):
-                   print(identification, courier)
-                   
-               print("\n")   
-               courier_selection = input("Enter the identification number of the courier you wish to select: ")
-               try: 
-                    courier_identification = int(courier_selection)
-                    if courier_identification > len(couriers) - 1: # Checks for valid index range in list
-                        raise IndexError
-                    elif courier_identification < 0: # Also checks for valid index range in list
-                        raise IndexError
-                    
-               except IndexError:
-                   print("Sorry, that number isn't a valid choice for a courier. Please try again.")
-               except Exception as e:
-                   print(f"Error: {e}. Try again.")
-
-               else:
-                    orders_list.append({"Customers' Name": customers_name, "Customers' Address": customers_address,\
-                        "Customers' Phone Number": customers_contact, "Courier": courier_identification,\
-                            "Status": "Preparing"})
-                    print("Your order has been placed.")
-        
+            create_order(couriers, orders_list)
         
         elif fourth_choice == "3":
             # Update existing order status
-            
-            for location, order in enumerate(orders_list):
-                print(location, order)
-            print("\n")
-            order_index_value = input("Which order would you like to edit? Please enter a number listed above: ")
-            
-            for space, order in enumerate(orders_status):
-                print(space, order)
-            print("\n")
-            orders_status_index = input("Please enter the number of the status you'd like to set: ")
-            
-            try:
-                orders_list[int(order_index_value)]["Status"] = orders_status[int(orders_status_index)]
-            except Exception as e:
-                print(f"Error: {e}. Please try again.")
-            else:
-                print("Your orders' status has been updated. Here is your new order:")
-                print(orders_list[int(order_index_value)])
-                
+            update_order_status(orders_list, orders_status)
+
         elif fourth_choice == "4":
             # Update existing order
-            #pass
-            for placement, order in enumerate(orders_list):
-                print(placement, order)
-            print("\n")
-            
-            selected_order = input("Which order would you like to update?: ").title()
-            print("\n")
-    
-            try:
-                updated_orders = orders_list[int(selected_order)]
-                
-            except Exception as e:
-                print(f"Error: {e}, Please try again.")
-                
-            else:
-                for key, value in updated_orders.items():
-                    print(f"Curerent Key: {key}, Current Value: {value}")
-                    print("\n")
-                    value_change = input("What would you like the new this new value to be?: ")
-                    if value_change == "":
-                        print("\n")
-                        print("You haven't inputted anything to change the value")
-                    else:
-                        orders_list[int(selected_order)][key] = value_change
+            update_existing_order(orders_list)
             
         elif fourth_choice == "5":
             # Delete an order
-            
-            for placement, order in enumerate(orders_list):
-                print(placement, order)
-                
-            print("\n")    
-            remove_item = input("Enter the number of the order you'd like to delete: ")
-            
-            try:
-                del orders_list[int(remove_item)]
-                
-            except Exception as e:
-                print(f"Error: {e}, please try again.")
-                
-            else:
-                print("You've successfully removed the order.")
-                print("\n")
+            delete_order(orders_list)
+
         else:
             print(INVALID)
     else:
