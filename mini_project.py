6ad5e200b70d3abd4c97642cf67e23f01672bf58
jaltpi <jenielthomas@gmail.com>
2021-07-20 18:29:00 +0100

    Replaced first choice to main menu choice, second choice to products menu choice, third choice to courier menu choice, and fourth choice to orders menu choice.

--- mini_project.py
+++ mini_project.py
@@ -21,9 +21,9 @@ application_on = True
 while application_on:
     main_menu()
     
-    first_choice = input("What would you like todo?\nEnter a number listed above to proceed or enter 'Exit' to quit: ").title()
+    main_menu_choice = input("What would you like todo?\nEnter a number listed above to proceed or enter 'Exit' to quit: ").title()
     
-    if first_choice == "0" or first_choice == "Exit":
+    if main_menu_choice == "0" or main_menu_choice == "Exit":
         # Exit Application and Save Data
         with open("products.txt", "w") as updated_products:
             for item in products:
@@ -38,90 +38,90 @@ while application_on:
         exit_consent = input("Please press 'Enter' to exit.")
         exit()
         
-    elif first_choice == "1":
+    elif main_menu_choice == "1":
         # PRODUCTS MENU
         products_menu()
         
-        second_choice = input("Please enter a number listed above: ")
-        if second_choice == "0":
+        products_menu_choice = input("Please enter a number listed above: ")
+        if products_menu_choice == "0":
             # Return to main menu
             print(LOADING)
             
-        elif second_choice == "1":
+        elif products_menu_choice == "1":
             # Shows current list
             show_current_list(products)
                 
-        elif second_choice == "2":
+        elif products_menu_choice == "2":
             # Create a new entry in the list
             create_new_item(products)
             
-        elif second_choice == "3":
+        elif products_menu_choice == "3":
             # Edit an object in the list
             edit_existing_item(products)
         
-        elif second_choice == "4":
+        elif products_menu_choice == "4":
             # Delete an object in the list
             delete_existing_item(products)
         
         else:
             print(INVALID)   
         
-    elif first_choice == "2":
+    elif main_menu_choice == "2":
         # COURIERS MENU
         couriers_menu()
         
-        third_choice = input("Please enter a number listed above: ")
+        courier_menus_choice = input("Please enter a number listed above: ")
         
-        if third_choice == "0":
+        if courier_menus_choice == "0":
             # Return to main menu
             print(LOADING)
             
-        elif third_choice == "1":
+        elif courier_menus_choice == "1":
             # Show current couriers list
             show_current_list(couriers)
             
-        elif third_choice == "2":
+        elif courier_menus_choice == "2":
             # Create new courier
             create_new_item(couriers)
         
-        elif third_choice == "3":
+        elif courier_menus_choice == "3":
             # Update existing courier   
             edit_existing_item(couriers)
         
-        elif third_choice == "4":
+        elif courier_menus_choice == "4":
             # Delete an existing courier
             delete_existing_item(couriers)
         
         else:
             print(INVALID)
                 
-    elif first_choice == "3":
+    elif main_menu_choice == "3":
         # ORDERS' MENU
         orders_menu()   
         
-        fourth_choice = input("Please enter a number listed above: ")
+        orders_menu_choice = input("Please enter a number listed above: ")
         
-        if fourth_choice == "0":
+        if orders_menu_choice == "0":
             # Return to main menu
             print(LOADING)
             
-        elif fourth_choice == "1":
+        elif orders_menu_choice == "1":
             # Show current orders
             show_existing_orders(orders_list)
                             
-        elif fourth_choice == "2":
+        elif orders_menu_choice == "2":
             # Create an order
             create_new_order(couriers, orders_list)
         
-        elif fourth_choice == "3":
+        elif orders_menu_choice == "3":
             # Update existing order status
             update_order_status(orders_list, orders_status)
 
-        elif fourth_choice == "4":
+        elif orders_menu_choice == "4":
             # Update existing order
             update_existing_order(orders_list)
             
-        elif fourth_choice == "5":
+        elif orders_menu_choice == "5":
             # Delete an order
             delete_existing_order(orders_list)
 
