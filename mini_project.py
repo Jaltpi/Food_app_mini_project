00ed6c275fd65e6581e6912dde7078df0ee2b8d4
jaltpi <jenielthomas@gmail.com>
2021-07-21 21:03:46 +0100

    created cvs files (products, couriers, orders), data now loads from cvs files instead of text files, changed function 'create new item' to 'create new product' and 'create new courier'

--- mini_project.py
+++ mini_project.py
@@ -1,20 +1,23 @@
-# mini_project_week_3
+# mini_project
 
-from module import main_menu, orders_menu, products_menu, couriers_menu, create_new_item, show_current_list, edit_existing_item, delete_existing_item, update_existing_order
-from module import orders_menu, show_existing_orders, create_new_order, update_order_status, update_existing_order, delete_existing_order
+from module import main_menu, products_menu, couriers_menu, orders_menu, show_current_list
+from module import create_new_product, create_new_courier, create_new_order, update_order_status, update_existing_item
+from module import delete_existing_item
 
 LOADING = "Loading Main Menu..."
 INVALID = "Invalid selection, returning back to main menu."
 
-# Load data from .txt files  
-with open("products.txt") as products_list:
+# Load data from .cvs files  
+with open("products.cvs") as products_list:
     products = [line.strip() for line in products_list]
 
-with open("couriers.txt") as couriers_list:
+with open("couriers.cvs") as couriers_list:
     couriers = [line.strip() for line in couriers_list]
+
+with open("orders.cvs") as orders_list:
+    orders = [line.strip() for line in orders_list]
     
 orders_status = ["Order pending", "Preparing", "On it's way", "Delievered"]
-orders_list = []
     
 application_on = True
 
@@ -25,13 +28,17 @@ while application_on:
     
     if main_menu_choice == "0" or main_menu_choice == "Exit":
         # Exit Application and Save Data
-        with open("products.txt", "w") as updated_products:
+        with open("products.cvs", "w") as updated_products:
             for item in products:
                 updated_products.write("%s\n" % item)
         
-        with open("couriers.txt", "w") as updated_couriers:
+        with open("couriers.cvs", "w") as updated_couriers:
             for item in couriers:
                 updated_couriers.write("%s\n" % item)
+                
+        with open("orders.cvs", "w") as updated_orders:
+            for item in orders:
+                updated_orders.write("%s\n" % item)
                 
         application_on = False
         print("Have a good day, thank you for using our app!")
@@ -53,11 +60,11 @@ while application_on:
                 
         elif products_menu_choice == "2":
             # Create a new entry in the list
-            create_new_item(products)
+            create_new_product(products)
             
         elif products_menu_choice == "3":
             # Edit an object in the list
-            edit_existing_item(products)
+            update_existing_item(products)
         
         elif products_menu_choice == "4":
             # Delete an object in the list
@@ -82,11 +89,11 @@ while application_on:
             
         elif courier_menus_choice == "2":
             # Create new courier
-            create_new_item(couriers)
+            create_new_courier(couriers)
         
         elif courier_menus_choice == "3":
             # Update existing courier   
-            edit_existing_item(couriers)
+            update_existing_item(couriers)
         
         elif courier_menus_choice == "4":
             # Delete an existing courier
@@ -107,23 +114,23 @@ while application_on:
             
         elif orders_menu_choice == "1":
             # Show current orders
-            show_existing_orders(orders_list)
+            show_current_list(orders)
                             
         elif orders_menu_choice == "2":
             # Create an order
-            create_new_order(couriers, orders_list)
+            create_new_order(couriers, products, orders)
         
         elif orders_menu_choice == "3":
             # Update existing order status
-            update_order_status(orders_list, orders_status)
+            update_order_status(orders, orders_status)
 
         elif orders_menu_choice == "4":
             # Update existing order
-            update_existing_order(orders_list)
+            update_existing_item(orders)
             
         elif orders_menu_choice == "5":
             # Delete an order
-            delete_existing_order(orders_list)
+            delete_existing_item(orders)
 
         else:
             print(INVALID)
